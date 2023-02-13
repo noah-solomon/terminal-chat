9ddb0d75fe75f9784308137fec349f7f31f998f7
Noah Solomon <njs99@cornell.edu>
2023-02-12 22:26:21 -0500

    add gitignore

--- TerminalChat/setup.py
+++ TerminalChat/setup.py
@@ -1,9 +1,22 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read()
+
 
 setup(
     name='tchat',
     version='0.1.0',
-    py_modules=['tchat'],
+    author='Noah Solomon',
+    author_email='hello@noahsolo.com',
+    license='MIT License',
+    description='Terminal chat application',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/noah-solomon/terminal-chat',
+    py_modules=['tchat', 'app'],
     install_requires=[
         'Click',
         'requests'
